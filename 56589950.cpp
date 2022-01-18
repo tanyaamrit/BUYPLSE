055e0f1fb0418f2d1e1786ca70442536b03e2da5
tanyaamrit <81899412+tanyaamrit@users.noreply.github.com>
2022-01-18 20:22:43 +0530

    Add files via upload

---
+++ 56589950.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,x,y,total;
+    cin>>a>>b>>x>>y;
+    total=(a*x)+(b*y);
+    cout<<total;
+    
+	// your code goes here
+	return 0;
+}
